d1d49bdc8046cb6fe46d4de664678a0b1f35041a
Sergey Kirpik <sergeykirpik@gmail.com>
2020-08-24 16:35:58 +0300

    Submit form, fix color-swatch.

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -1,21 +1,28 @@
-import { makeGradient } from './color_utils';
+import { setElementColor } from './color_utils';
 
-import { toLocalISOTime, toLocalISODate } from './date_utils';
+import { padWithZero } from './date_utils';
+
+import { toLocalISOTime, toLocalISODate, toLocalISOTimeWithoutSeconds } from './date_utils';
+import { deselectAllIntervals } from './calendar';
+
+import { randomColor, randomInt } from './random';
 
 function fillDialog(dialog, data) {
     console.log(data);
 
     dialog.querySelector('.status').textContent = `[ ${data['status']} ]`;
     const form = dialog.querySelector('form');
+    form.eventId.value = data['id'];
     form.title.value = data['title'];
     form.description.value = data['description'];
     form.startDate.value = toLocalISODate(data['startDate']);
     form.endDate.value = toLocalISODate(data['endDate']);
 
-    form.startTime.value = toLocalISOTime(data['startDate']);
-    form.endTime.value = toLocalISOTime(data['endDate']);
+    form.startTime.value = toLocalISOTimeWithoutSeconds(data['startDate']);
+    form.endTime.value = toLocalISOTimeWithoutSeconds(data['endDate']);
 
-    form.querySelector('.color-swatch').style.background = makeGradient(data['color']);
+    form.color.value = data['color'];
+    setElementColor(form.querySelector('.color-swatch'), data['color']);
     form.author.value = data['author'];
 }
 
@@ -43,7 +50,7 @@ function hideOnTransitionComplete(e) {
 
 function closeDialog() {
     const dialog = document.querySelector('.dialog')
-    if (dialog) {
+    if (dialog && !dialog.classList.contains('transparent')) {
         console.log('close');
         dialog.classList.add('transparent');
         dialog.addEventListener('transitionend', hideOnTransitionComplete);
@@ -73,6 +80,46 @@ function makeDraggable(dialog) {
     });
 }
 
+function updateInterval(data) {
+    const el = document.querySelector(`[data-id="${data.id}"]`);
+    el.textContent = data.title;
+    setElementColor(el, data.color);
+}
+
+function submitHandler(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    console.log('submit form...');
+
+    const data = {
+        title: formData.get('title'),
+        description: formData.get('description'),
+        startDate: new Date(formData.get('startDate') + 'T' + formData.get('startTime')),
+        endDate: new Date(formData.get('endDate') + 'T' + formData.get('endTime')),
+        color: formData.get('color'),
+    };
+    const eventId = formData.get('eventId');
+
+    fetch(`/api/events/${eventId}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        updateInterval(data.data);
+    });
+
+    closeDialog();
+    deselectAllIntervals();
+}
+
+function handleColorChange(e) {
+    console.log(e.target.value);
+    setElementColor(document.querySelector('.dialog .color-swatch'), e.target.value);
+}
+
 function setupDialogs() {
 
     document.querySelector('.dialog .btn-close')
@@ -82,6 +129,10 @@ function setupDialogs() {
     document.querySelectorAll('.dialog').forEach(dialog => {
         makeDraggable(dialog);
     });
+
+    document.querySelector('.dialog form').addEventListener('submit', submitHandler);
+
+    document.querySelector('.dialog #color').addEventListener('change', handleColorChange);
 }
 
 
